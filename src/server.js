cd26ac0067a29c1d0430241c2c5525bd1daa39ea
unknown <yyongkisetiawan@gmail.com>
2024-04-21 22:30:24 +0700

    fix error

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 const ClientError = require('./exceptions/ClientError');
 
@@ -47,9 +48,14 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: Jwt,
-  });
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+    {
+      plugin: Inert,
+    },
+  ]);
 
   server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
